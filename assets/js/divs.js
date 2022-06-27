f08918ab1b470764b49c6c8a893880078781d422
wasdthedebugger <wasdthedebugger@gmail.com>
2022-06-27 22:09:10 +0545

    Added Files

---
+++ assets/js/divs.js
@@ -0,0 +1,36 @@
+function code() {
+    if (document.getElementById('vscode').style.display == "none") {
+        document.getElementById('vscode').style.display = "block";
+    } else {
+        document.getElementById('vscode').style.display = "none";
+    }
+};
+
+function menu() {
+    if (document.getElementById('menu').style.display == "none") {
+        document.getElementById('menu').style.display = "block";
+    } else {
+        document.getElementById('menu').style.display = "none";
+    }
+};
+
+function hide() {
+    document.getElementById('menu').style.display = "none";
+    document.getElementById('vscode').style.display = "none";
+}
+
+function actionbar() {
+    if (document.getElementById('actionbar').style.display == "none") {
+        document.getElementById('actionbar').style.display = "block";
+    } else {
+        document.getElementById('actionbar').style.display = "none";
+    }
+}
+
+function minecraft() {
+    if (document.getElementById('minecraft').style.display == "none") {
+        document.getElementById('minecraft').style.display = "block";
+    } else {
+        document.getElementById('minecraft').style.display = "none";
+    }
+}
