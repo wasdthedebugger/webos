d80521e87469e1fec6697614bc281662a76b0a41
wasdthedebugger <93481198+wasdthedebugger@users.noreply.github.com>
2022-07-01 15:43:40 +0545

    PWA

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(['./', './assets/icons/192.png', './assets/icons/512.png']);
+        })
+    );
+    console.log("Installed");
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
