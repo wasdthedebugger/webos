1b7adc924cdb52449ca46174c3d2a4fac06417c6
Nikas Ghimire <wasdthedebugger@gmail.com>
2023-05-05 18:30:26 +0545

    Adding all statics to cache

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(['./', './assets/icons/192.png', './assets/icons/512.png', './assets/icons/favicon.png']);
+            return cache.addAll([
+                "**/*",
+            ]);
         })
     );
-    console.log("Installed");
+    // console.log("Installed");
 });
 
 self.addEventListener("fetch", e => {
